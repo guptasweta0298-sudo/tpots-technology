d56d8c5418133216238023dd91023605940e581d
Sweta Gupta <gupta.sweta0298@gmail.com>
2026-07-14 23:37:40 +0530

    update

--- wp-content/themes/tpotstech/index.php
+++ wp-content/themes/tpotstech/index.php
@@ -49,7 +49,7 @@
             <div class="rb-backdrop__heading">
               <hgroup class="rb-lockupgroup">
                 <h1 class="rb-lockup alumni-lockup text-white">
-                 <?php echo wp_kses_post($banner_title); ?> || And
+                 <?php echo wp_kses_post($banner_title); ?>
                 </h1>
 
                 <p class="rb-lockup text-rb--font-size--xl normal-case">
